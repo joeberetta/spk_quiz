ddd696c9ceaae0310d5121e0b9339add8b6b9fb4
joeberetta <shakhrukh.97@gmail.com>
2019-10-28 15:41:19 +0700

    Questions db added

---
+++ data.js
@@ -0,0 +1,164 @@
+const questions = [
+	{
+		question: 'В каком году был основан ТУСУР?',
+		answers: [
+			{ answer: 1972, score: 0 },
+			{ answer: 1978, score: 0 },
+			{ answer: 1962, score: 1 },
+			{ answer: 1970, score: 0 }
+		]
+	},
+	{
+		question: 'Кто явлеяется действующим ректором ТУСУРа?',
+		answers: [
+			{ answer: 'Шелупанов', score: 0 },
+			{ answer: 'Рулевский', score: 0 },
+			{ answer: 'Шурыгин', score: 0 }
+		]
+	},
+	{
+		question: 'Сколько всего факультетов в ТУСУРе?',
+		answers: [
+			{ answer: 12, score: 1 },
+			{ answer: 11, score: 0 },
+			{ answer: 10, score: 0 },
+			{ answer: 9, score: 0 }
+		]
+	},
+	{
+		question: 'В каком кабинете главного корпуса находится Профком студентов?',
+		answers: [
+			{ answer: 236, score: 1 },
+			{ answer: 235, score: 0 },
+			{ answer: 102, score: 0 },
+			{ answer: 113, score: 0 }
+		]
+	},
+	{
+		question: 'В состав какого университета изначально входил ТУСУР?',
+		answers: [
+			{ answer: 'ТПУ', score: 1 },
+			{ answer: 'ТГПУ', score: 0 },
+			{ answer: 'ТГУ', score: 0 },
+			{ answer: 'ТГАСУ', score: 0 }
+		]
+	},
+	{
+		question: 'Когда отмечает ТУСУР отмечает свой главный праздник?',
+		answers: [
+			{ answer: '7 мая', score: 1 },
+			{ answer: '9 мая', score: 0 },
+			{ answer: '1 сентября', score: 0 },
+			{ answer: '12 июня', score: 0 }
+		]
+	},
+	{
+		question: 'Академическая стипендия выплачивается из средств:',
+		answers: [
+			{ answer: 'федерального бюджета', score: 1 },
+			{ answer: 'ректорского фонда', score: 0 },
+			{ answer: 'фонда Профсоюзной организации студентов', score: 0 }
+		]
+	},
+	{
+		question:
+			'По какому виду деятельности нельзя получить повышенную стипендию?',
+		answers: [
+			{ answer: 'волонтерская', score: 1 },
+			{ answer: 'учебная', score: 0 },
+			{ answer: 'научная', score: 0 },
+			{ answer: 'творческая', score: 0 }
+		]
+	},
+	{
+		question: 'Повышенную стипендию можно получать сразу:',
+		answers: [
+			{ answer: 'по 5 направлениям', score: 1 },
+			{ answer: 'по 2 направлениям', score: 0 },
+			{ answer: 'по 3 направлениям', score: 0 },
+			{ answer: 'по 4 направлениям', score: 0 },
+			{ answer: 'только по 1 направлению', score: 0 }
+		]
+	},
+	{
+		question: 'Районный коэффициент Томской области:',
+		answers: [
+			{ answer: 1.3, score: 1 },
+			{ answer: 1.4, score: 0 },
+			{ answer: 1.7, score: 0 },
+			{ answer: 1.7, score: 0 }
+		]
+	},
+	{
+		question: 'Социальная стипендия выплачивается:',
+		answers: [
+			{ answer: 'отличникам', score: 0 },
+			{ answer: 'малоимущим', score: 0 },
+			{ answer: 'иностранным студентам', score: 0 },
+			{ answer: 'нуждающимся студентам', score: 1 }
+		]
+	},
+	{
+		question: 'Размеры стипендий устанавливает:',
+		answers: [
+			{ answer: 'ТУСУР', score: 1 },
+			{ answer: 'Президент', score: 0 },
+			{ answer: 'Правительство', score: 0 },
+			{ answer: 'Профком', score: 0 }
+		]
+	},
+	{
+		question: 'Какой % четверок допускается в красном дипломе?',
+		answers: [
+			{ answer: 25, score: 1 },
+			{ answer: 30, score: 0 },
+			{ answer: 10, score: 0 },
+			{ answer: 15, score: 0 }
+		]
+	},
+	{
+		question: 'Базовый размер академической стипендии:',
+		answers: [
+			{ answer: 1697, score: 1 },
+			{ answer: 2354, score: 0 },
+			{ answer: 3426, score: 0 },
+			{ answer: 1567, score: 0 }
+		]
+	},
+	{
+		question: 'К промежуточной аттестации не относится:',
+		answers: [
+			{ answer: 'лабораторная', score: 1 },
+			{ answer: 'зачет', score: 0 },
+			{ answer: 'экзамен', score: 0 },
+			{ answer: 'курсовая', score: 0 }
+		]
+	},
+	{
+		question: 'К именным стипендиям ТУСУРа относятся стипендии:',
+		answers: [
+			{ answer: 'им. Перегудова', score: 0 },
+			{ answer: 'им. Гюнтера', score: 0 },
+			{ answer: 'им. Зубарева', score: 0 },
+			{ answer: 'все ответы верны', score: 1 }
+		]
+	},
+	{
+		question: 'Выплата социальной стипендии прекращается в случае:',
+		answers: [
+			{ answer: 'отчисления', score: 1 },
+			{ answer: 'получения 3 за сессию', score: 0 },
+			{ answer: 'получения повышенной стипендии', score: 0 },
+			{ answer: 'перевода на другой факультет', score: 0 }
+		]
+	},
+	{
+		question: 'Материальную помощь можно получить от:',
+		answers: [
+			{ answer: 'Профкома', score: 0 },
+			{ answer: 'деканата', score: 0 },
+			{ answer: 'ректора', score: 0 },
+			{ answer: 'все ответы верны', score: 1 }
+		]
+	}
+];
