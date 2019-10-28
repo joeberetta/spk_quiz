0a86f8ae6736ee01c126d1b7e2e7e65344d42043
joeberetta <shakhrukh.97@gmail.com>
2019-10-28 14:43:28 +0700

    Minor fixes

--- quiz.js
+++ quiz.js
@@ -80,7 +80,7 @@ class Quiz {
 		vkShareBtn.id = 'vkShareBtn';
 		vkShareBtn.innerHTML = VK.Share.button(
 			{
-				url: 'https://joeberetta.github.io/spk-quizr',
+				url: 'https://joeberetta.github.io/spk_quiz/index.html',
 				title: 'Онлайн-квиз от СПК',
 				image: `http://via.placeholder.com/2000x400/fff/000.jpg?text=%D0%9C%D0%BE%D0%B9+%D1%80%D0%B5%D0%B7%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%82%3A+${this.score}+%D0%B8%D0%B7+${this.totalScore}`,
 				noparse: true
